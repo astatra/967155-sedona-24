8f6f4e42347bca715387cc3c6634c186d0c2fab9
Hanna Rul <anna.astatra@gmail.com>
2019-03-06 01:34:41 +0100

    Apply changes and add js

---
+++ js/external.js
@@ -0,0 +1,21 @@
+// @ts-check
+
+var form = document.querySelector('.bookingForm');
+var button = document.querySelector('.promo');
+var input = form.querySelector('.input');
+var checkOut = form.querySelector('.checkOut');
+var adultsNumber = form.querySelector('.inputNumber');
+var childrenNumber = form.querySelector('.children');
+
+button.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    form.classList.toggle('bookingForm-show');
+    input.focus();
+    input.selectionStart = input.value.length;
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!input.value || !checkOut.value || !adultsNumber.value || !childrenNumber.value) {
+        evt.preventDefault();
+    }
+});
